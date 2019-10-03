dbf576153267a7ed86e256b2c45397d20acf30f8
Elciney Junior <elcineyjr123@gmail.com>
2019-10-03 13:58:28 -0300

    exec com as flags funcionando

--- utils/chamadaSistema.c
+++ utils/chamadaSistema.c
@@ -7,7 +7,7 @@ void resolveComandos(char*** comandos){
     adicionaCaminho(comandos);
     for(int i = 0; i < 5; i++){
         if(!comandos[i]) break;
-        char *argv[]={comandos[i][0], NULL};
+        char *argv[]={comandos[i][0], comandos[i][1], comandos[i][2], comandos[i][3], comandos[i][4], NULL};
         char *env_args[] = {"PATH=/bin", (char*)0};
         execve(argv[0],argv,env_args);    
     }
